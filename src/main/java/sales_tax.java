36421cb505e55e8704704fdacacb659447a6db14
Whit3Flam3 <50198595+Whit3Flam3@users.noreply.github.com>
2019-09-05 16:37:12 -0500

    Project: sales_tax
    Commit 4
    Notes: Reverted the code back to it's original state from "Commit 2"

--- src/main/java/sales_tax.java
+++ src/main/java/sales_tax.java
@@ -28,7 +28,7 @@ public class sales_tax {
     float totalTax = (countyTaxPrice + stateTaxPrice);
     float totalSale = (totalTax + priceAmount);
     
-    System.out.println("County tax amount: $" + (System.out.printf("%.2f", countyTaxPrice)) + "\n" + "State tax amount: $" + (System.out.printf("%.2f", stateTaxPrice)) + "\n" + "Total tax amount: $" + (System.out.printf("%.2f", totalTax)) + "\n" + "Total sale amount: $" + (System.out.printf("%.2f", totalSale)));
+    System.out.println("County tax amount: $" + countyTaxPrice + "\n" + "State tax amount: $" + stateTaxPrice + "\n" + "Total tax amount: $" + totalTax + "\n" + "Total sale amount: $" + totalSale);
     
     }
 }
